53ae19790e35687cbae271c15da59fb577e333d0
bhargav1258 <mspkbhargav000@gmail.com>
2026-06-01 16:15:10 +0530

    changes in testng.xml

--- src/test/java/StepDef/RunnerTest.java
+++ src/test/java/StepDef/RunnerTest.java
@@ -7,7 +7,7 @@ import io.cucumber.testng.CucumberOptions;
 @CucumberOptions(
         features = "src/test/resources/features",
         glue = "StepDef",
-       // tags = "@regression", 
+       tags = "@regression", 
         plugin = {
                 "pretty",
                 "html:target/cucumber-reports/report.html",
